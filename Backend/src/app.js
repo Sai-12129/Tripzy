a65d537f2724e7a07103e21515d10aaa66bb9d7c
Lakshmikar <saichowdari22@gmail.com>
2026-05-11 18:41:46 +0530

    Fixed render Deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv"
+dotenv.config()
 import express from "express"
 import cors from "cors"
 import userRouter from "./routers/user.route.js";
@@ -15,19 +17,19 @@ app.use(cors({
 }));
 
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
 
 
-app.get('/',(req,res)=>{
-    res.send("Hello World!!")
+app.get('/', (req, res) => {
+  res.send("Hello World!!")
 })
 
-app.use("/users",userRouter);
-app.use("/captains",captainRouter)
-app.use('/maps',mapRouter)
-app.use('/rides',rideRouter)
+app.use("/users", userRouter);
+app.use("/captains", captainRouter)
+app.use('/maps', mapRouter)
+app.use('/rides', rideRouter)
 
 
-export{app}
+export { app }
